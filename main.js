8f483b5c74c9eed16b7236cf3ed4aa2ab3a64d35
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2022-04-20 00:31:55 +0200

    Click on image open carousel modal
    - Add & style carousel modal.
    - Click on image open carousel modal.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const galleryEl = document.querySelector("#gallery");
+const modal = document.querySelector("#modal");
+const modalImg = document.querySelector("#modal-img");
 let imagesArr;
 
 class Image {
@@ -10,7 +12,7 @@ class Image {
 
     get imgHTML() {
         return `<div class="img-container">
-                    <img src=${this.url} alt="" />
+                    <img class="image" src=${this.url} alt="" />
                     <span class="material-icons-outlined zoom-icon">search</span>
                 </div>`;
     }
@@ -22,3 +24,36 @@ fetch("https://scaleflex.cloudimg.io/v7/0.fe_task_static/pictures.json?vh=7a646d
         imagesArr = data.map((image) => new Image(image.uuid, image.name, image.url));
         galleryEl.innerHTML = imagesArr.map((image) => image.imgHTML).join("");
     });
+
+galleryEl.addEventListener("click", function (event) {
+    modal.showModal();
+    if (event.target.classList.contains("zoom-icon")) {
+        const zoomIconsArr = Array.from(document.getElementsByClassName("zoom-icon"));
+        const zoomIconIndex = zoomIconsArr.indexOf(event.target);
+        displayModalImg(imagesArr[zoomIconIndex]);
+    } else if (event.target.classList.contains("image")) {
+        const clickedImgSrc = event.target.src;
+        imagesArr.forEach((image) => {
+            if (image.url === clickedImgSrc) {
+                displayModalImg(image);
+            }
+        });
+    }
+});
+
+modal.addEventListener("click", function (event) {
+    if (event.target.classList.contains("modal-img")) {
+        return;
+    }
+    modal.close();
+});
+
+// Function to display the clicked image in modal
+function displayModalImg(image) {
+    const imgCount = document.querySelector(".img-count");
+    modalImg.innerHTML = `
+        <img class="modal-img" src=${image.url} alt="" />
+        <figcaption class="img-name">${image.name}</figcaption>
+    `;
+    imgCount.textContent = `Image ${imagesArr.indexOf(image) + 1}/${imagesArr.length}`;
+}
